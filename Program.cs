5579f199e20f26ba4a9087b54e595a040c7f085d
Fabian <prime43@gmx.de>
2018-03-07 17:10:08 +0100

    hey sexy
    Signed-off-by: Fabian <prime43@gmx.de>

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hallo_welt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I CANT DO SHIT!");
+        }
+    }
+}
